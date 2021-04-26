0a9ce80df2a262b37a64c578a98df8621b452b4e
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-04-26 02:25:30 -0400

    deleted old code in sql file

--- db/tech-blog.sql
+++ db/tech-blog.sql
@@ -1,34 +1,3 @@
-drop database if exists ecommerce_db;
-create database if not exists ecommerce_db;
-use ecommerce_db;
-
-create table Category (
-	id int not null auto_increment,
-    category_name varchar(30),
-    primary key (id)
-);
-
-create table Product (
-	id int not null auto_increment,
-    product_name varchar(30),
-    price decimal(2,2) not null,
-    stock int not null,
-    category_id int,
-    primary key (id),
-    foreign key (category_id) references Category(id)
-);
-
-create table Tag (
-	id int not null auto_increment,
-    tag_name varchar(30),
-    primary key (id)
-);
-
-create table ProductTag (
-	id int not null auto_increment,
-    product_id int,
-    tag_id int,
-    primary key (id),
-    foreign key (product_id) references Product(id),
-    foreign key (tag_id) references Tag(id)
-);
+drop database if exists techblog_db;
+create database if not exists techblog_db;
+use techblog_db;
