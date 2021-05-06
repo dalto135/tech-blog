cf26e2d2ed9b2228c82a9892171df1e366ab7ac6
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-05-05 21:40:41 -0400

    overhauled starter code to begin work on assignment, project.js is now post.js, created comment.js, created comment seed data, updated routes, replaced every occurance of the word "project" in the repo with the word "post"

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,3 @@
 drop database if exists techblog_db;
 create database if not exists techblog_db;
 use techblog_db;
-
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
-use crowdfund_db;
--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Comment extends Model {}
 
-Project.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,26 +11,27 @@ Project.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    content: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
-      type: DataTypes.STRING,
-    },
+    
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    user_id: {
+    author_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
         key: 'id',
       },
     },
@@ -40,8 +41,8 @@ Project.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'post',
   }
 );
 
-module.exports = Project;
+module.exports = Comment;
