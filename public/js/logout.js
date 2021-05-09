d56fa5d6c5d6d04e61ebb3dafa86c460a94efaa0
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-05-08 22:21:40 -0400

    the user is logged out if their screen is idle for 5 minutes

--- public/js/logout.js
+++ public/js/logout.js
@@ -16,4 +16,25 @@ const logout = async () => {
 }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+var idleTime = 0;
+var idleInterval = setInterval(timerIncrement, 60000);
+
+document.addEventListener('mousemove', function() {
+  idleTime = 0;
+})
+      
+document.addEventListener('keydown', function() {
+  idleTime = 0;
+});
+
+//The user is logged out if their screen is idle for 5 minutes
+function timerIncrement() {
+  idleTime = idleTime + 1;
+  if (idleTime > 5) {
+    logout();
+  }
+}
+
+document
+  .querySelector('#logout')
+  .addEventListener('click', logout);
